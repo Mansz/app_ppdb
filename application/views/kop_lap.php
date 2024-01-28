70cc4ca4049f67e13d1d00012f9048ddbe211453
pusdatindo <pusdatindo@gmail.com>
2024-01-29 02:08:54 +0700

    New repo

---
+++ application/views/kop_lap.php
@@ -0,0 +1,28 @@
+<table border="0" width="100%">
+  <tr>
+    <td align="center">
+      <img src="img/unnamed.jpg" alt="logo2" width="60">
+    </td>
+    <td align="center">
+      <b style="font-size:23px;">PANITIA PENERIAMAAN PESERTA DIDIK BARU (PPDB)</b> <br>
+      <b style="font-size:30px;">SMK NURUL HAYYAH</b> <br>
+      <b style="font-size:20px;">TAHUN PELAJARAN <?php echo $thn_ppdb; ?> / <?php echo $thn_ppdb+1; ?></b>
+    </td>
+    <td align="center">
+      <img src="img/unnamed.jpg" alt="logo2" width="70">
+    </td>
+  </tr>
+  <tr>
+    <td colspan="3" align="center" style="font-size:15px;">
+      Sekretariat : Jl. Lingkar Provinsi No.07 Bulakelor, Sawah, Ketanggungan, Brebes Regency, Central Java 52263, Indonesia <img src="img/telp.jpg" alt="telp." style="margin-bottom:-5px;margin-right:-5px;"> 0735-450106 Kab.Brebes <img src="img/kode_pos.jpg" alt="Kode Pos." style="margin-bottom:-3px;margin-right:-5px;"> 32382
+      <br>
+      Website : https://ppdb.smkbisanuha.sch.id   e-mail : smknuhabisa@gmail.com
+    </td>
+  </tr>
+  <tr>
+    <td colspan="3" align="center">
+      <hr size="0" color="black" style="margin:0px;margin-bottom:1px;">
+      <hr size="2" color="black" style="margin:0px;">
+    </td>
+  </tr>
+</table>
